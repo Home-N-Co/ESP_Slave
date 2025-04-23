0fd33a3ef0629b1b25aa6f089d3d88e1bfb6a8cb
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2025-04-23 19:23:44 +0200

    init projet

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,3 @@
 //
 // Created by Kantin FAGNIART on 24/03/2025.
 //
-
-#include "Arduino.h"
-
-void setup() {
-    Serial.begin(115200);
-    Serial.println("Setup terminé !");
-}
-
-void loop() {
-    Serial.println("Hello World");
-    delay(1000);
-}
